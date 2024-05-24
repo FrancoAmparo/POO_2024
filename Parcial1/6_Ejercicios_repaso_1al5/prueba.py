4d9f1333a475310656b88fb7c648699918fd339c
FrancoAmparo <amparo_3141230066@utd.edu.mx>
2024-05-23 22:32:40 -0600

    prueba

--- Parcial1/6_Ejercicios_repaso_1al5/prueba.py
+++ Parcial1/6_Ejercicios_repaso_1al5/prueba.py
@@ -1,1 +1,10 @@
-#esta es una preba
+#esta es una prueba
+
+old=int(input("INGRESA TU EDAD"))
+
+
+while old>=1:
+     if old >= 18:
+         print("YA PUEDES VOTAR TILIN A JAJAJAJAJA")
+     else:
+         print("NEEL PERRO")
