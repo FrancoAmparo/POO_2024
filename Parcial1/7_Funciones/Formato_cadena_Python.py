6b340d7a18a45f7206ce98fa9dd5a43ff8e2f7bb
FrancoAmparo <amparo_3141230066@utd.edu.mx>
2024-06-03 00:01:31 -0600

    Formato_cadena_Python

--- Parcial1/7_Funciones/Formato_cadena_Python.py
+++ Parcial1/7_Funciones/Formato_cadena_Python.py
@@ -1,55 +1,42 @@
-# 1_CUERDAS EN PYTHON 
-
+#1_CUERDAS EN PYTHON 
 cadena_simple='Hola, Mundo!'
 cadena_doble="Hola, Mundo!"
-print(cadena_simple)
 print(cadena_doble)
-
-#2_CADENA MULTILINEA
-
-cadena_multilinea='''Esta es una cadena de varias lineas'''
-print(cadena_multilinea)
-
-#3_MARCADORES DE POSICION Y MODIFICADORES
-
+print(cadena_simple)
+print()
+#CADENA MULTILINEA
+cadena_mulltilinea='''Esta es una cadena de varias lineas'''
+print(cadena_mulltilinea)
+print()
+#2_MARCADORES DE POSICION Y MODIFICADORES
 nombre="Juan"
 edad=25
-texto="Me llamo {} y tengo {} años.".format(nombre, edad)
+texto="Me llamo {} y tengo {} años.".format(nombre, edad) 
 print(texto)
-
-#MODIFICADORES
-numero=3.14159
-print("El numero es {:.2f}".format(numero))
-
-#REALIZAR OPERACIONES EN F-STRINGS
-
-        #OPERACIONES
-a=5
+print()
+#3_REALIZAR OPERACIONES EN F-STRINGS 
+a=25
 b=10
 print(f"la suma de {a} y {b} es {a + b}")
-
-     #OTRO EJEMPLO
-name= Emilio
-edadd=22
-print(f"Me llamo {name} y el proximo año tendre {edadd + 1} años")
-
-#EJECUTAR FUNCIONES EN F-STRINGS 
-
+print()
+#4_EJECUTAR FUNCIONES EN F-STRINGS 
 def mayuscula(texto):
     return texto.upper()
 
-print(f"Texto en mayusculas: {mayuscula("hola")}")
+print(f'Texto en mayusculas: {mayuscula("hola")}')
+print()
+#5_FORMATO DE CADENA
+   #Metodo '.format()'
 
-#FORMATO DE CADENA
-#Metodo .format()
-
-nombree=Manuel
-cant_años=31
-texto="Me llamo {0} y tengo {1} años. {0} es un buen nombre.".format(nombree, cant_años)
-print(texto)
-
-#VALORES MULTIPLES
-
-nombres=["Ana", "luis", "Marta"]
-print("Los nombres son: {}, {}, y {}".format(nombres[0], nombres[1], nombres[2]))
+nombree="Manuel"
+edaad=22
+texto="Me llamo {0} y tengo {1} años. {0} es un buen nombre".format(nombree, edaad)
+print()
+#6_VALORES MULTIPLES
+nombres=["Ana", "Luis", "Marta"]
+print("Los nombres son: {}, {} y {}".format(nombres[0], nombres[1], nombres[2])) 
+print()
+#7_INDICES CON NOMBRE
+persona={"noombre": "Mario", "eedad":35}
+texto="Me llamo {noombre} y tengo {eedad}".format(**persona)
 
