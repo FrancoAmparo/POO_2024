e75c4d5332daad9a8e694bf6e4d64f415b8f3557
FrancoAmparo <amparo_3141230066@utd.edu.mx>
2024-06-05 22:37:15 -0600

    .

--- Parcial1/8_listas/listas.py
+++ Parcial1/8_listas/listas.py
@@ -28,35 +28,76 @@ son colecciones o conjunto de datos/valores bajo un mismo nombre, para acceder a
 #     i+=1
 
 
-#EJEMPLO 2
-#Crear una lista de palabras y posteriormente buscar la coincidencia de una palabra:
+#Ejemplo2
+#crear una lista de palabras, posteriormente buscar la coincidencia de una palabra
+# palabra = ["hola","utd", "como", "estas", "ok", "naranja"]
+# palabra_buscar = input("inserta palabra a buscar: ")
 
-palabras=["naranja", "utd", "america", "azul"]
+# if palabra_buscar in palabra:
+#     for i, p in enumerate(palabra):
+#         if p == palabra_buscar:
+#             print(f"Encontré la palabra en la posición {i+1}")
+# else:
+#     print("No encontré la palabra en la lista")
 
-palabra_buscar=input("Ingresa la palabra a buscar: ")
-i=0
-for i in palabras:
-    if i==palabra_buscar:
-        print(f"se encontro la palabra a buscar en la posicion:  {palabras.index}")
+
+#While
+#while
+# palabras = ["hola", "utd", "estas", "nada", "ok", "naranja"]
+# buscar = input("inserta palabra a buscar: ")
+# i = 0
+# encontrado = False
+# while i < len(palabras):
+#     if palabras[i] == buscar:
+#         encontrado = True
+#         break
+#     i +=2
+# if encontrado:
+#     print(f"La palabra '{buscar}' se encontró en la lista en la posición {i+1}.")
+# else:
+#     print(f"La palabra '{buscar}' no se encontró en la lista.")
 
 
-#Ejemplo 3 agregar y elimiinar elementos d euna lista
-#os.system("clear")
+#4_Crear una lista multilinea (matriz ) para agregar los nombres y numeros telefonicos 
+agenda=
+("carlos", 6181234567,
+leo=, 6671234573,
+"sebastia", 6181231234,
+"pedro", 618113334455
+)
+print(agenda)
+for i in agenda:
+print(f(agenda.index(i)+1),-(i))
 
-numeros =[23,34,23]
-print(numeros)
 
-#Agregar un elemento
-numeros.append(100)
-print(numeros)
-numeros.insert(3,200)
-print(numeros)
+#5_Crear un programa que permita gestionar (administrar) pelicula, coloca un menu de opciones
+#para agregar, remover, consultar peliculas
+#NOTAS:
+#1_utiliza funciones y mandar llamar desde otro archivo
+#2_utilizar listas para almacenar los nombres de peliculas
 
-#Eliminar un elemento 
-numeros.remove(100) #Indicar el elemto a borrar 
-print(numeros)
-numeros.pop(2)#Indicar la posicion de el elemnto a borrar 
-print(numeros)
+
+
+
+
+
+# #Ejemplo 3 agregar y elimiinar elementos de una lista
+# #os.system("clear")
+
+# numeros =[23,34,23]
+# print(numeros)
+
+# #Agregar un elemento
+# numeros.append(100)
+# print(numeros)
+# numeros.insert(3,200)
+# print(numeros)
+
+# #Eliminar un elemento 
+# numeros.remove(100) #Indicar el elemto a borrar 
+# print(numeros)
+# numeros.pop(2)#Indicar la posicion de el elemnto a borrar 
+# print(numeros)
 
 
 
